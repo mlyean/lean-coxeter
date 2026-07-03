3c5762a89c8b8a135f1aa67bbd4a6acb6ee533b2
Cobord <ammar.s.husain@gmail.com>
2026-07-03 15:24:35 -0400

    WIP hecke algebra

--- Coxeter.lean
+++ Coxeter.lean
@@ -1,11 +1,14 @@
 module
 
 public import Coxeter.Basic
+public import Coxeter.BraidGroup
 public import Coxeter.Bruhat
 public import Coxeter.Data.List.Lemmas
 public import Coxeter.GeometricRepresentation
+public import Coxeter.LinearAlgebra.AssociatedGraded
 public import Coxeter.LinearAlgebra.BilinearForm
 public import Coxeter.LinearAlgebra.TwoDim
 public import Coxeter.Order.Directed
 public import Coxeter.PermutationRepresentation
 public import Coxeter.StrongExchange
+public import Coxeter.Hecke
